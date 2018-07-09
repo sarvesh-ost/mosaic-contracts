e3ec01a98f43acf0212271877b52b9439856ba61
Sarvesh Jain <sarvesh@ost.com>
2018-07-09 20:19:37 +0530

    Addeded tests for multiple stake request and one unstake request

--- test/TokenConversionPrecision.js
+++ test/TokenConversionPrecision.js
@@ -6,7 +6,7 @@ const openstValueUtils = require('./OpenSTValue_utils')
 ;
 
 
-contract('Token conversion', function (accounts) {
+contract('Token conversion precision test', function (accounts) {
   const registrar = accounts[1];
 
 
@@ -45,7 +45,7 @@ contract('Token conversion', function (accounts) {
       await openSTValue.registerUtilityToken(symbol, name, conversionRate, conversionRateDecimals, chainIdUtility, staker, uuid, {from: registrar});
     }
 
-    it("Should receive expected number of branded token on stake", async () => {
+    it("Should receive expected number of branded token  on  single stake and  value token on unstake request", async () => {
 
       const amountST = 2;
 
@@ -53,13 +53,68 @@ contract('Token conversion', function (accounts) {
       let hashLock = lock.l
         , unlockHash = lock.s;
 
-      let decodedStakeEvent = await tokenConversionUtils.processStake(valueToken, openSTValue, amountST, staker, uuid, hashLock, unlockHash);
-
+      let decodedStakeEvent = await tokenConversionUtils.processStake(valueToken, openSTValue, amountST, staker, uuid, hashLock, unlockHash)
+        , expectedBTValue = (amountST * conversionRate) / 10 ** conversionRateDecimals;
+      assert.equal(decodedStakeEvent.BTValue, expectedBTValue, `Minted BT tokens are ${decodedStakeEvent.BTValue} but expected value is ${expectedBTValue}`);
       let BTValue = decodedStakeEvent.BTValue
         , redeemNonce = decodedStakeEvent.nonce + 1;
 
       let decodeUnStakeEvent = await tokenConversionUtils.processUnstake(openSTValue, uuid, staker, redeemNonce, BTValue, hashLock, registrar);
       assert.equal(amountST, decodeUnStakeEvent.STValue, `Staked value token ${amountST} is not equal to un-staked value token ${decodeUnStakeEvent.STValue}`)
+    })
+
+
+    it("Should receive expected number of value token on unstake request of BT value less then one value token ", async () => {
+
+      const amountST = 2;
+
+      const lock = HashLock.getHashLock();
+      let hashLock = lock.l
+        , unlockHash = lock.s;
+
+      let decodedStakeEvent = await tokenConversionUtils.processStake(valueToken, openSTValue, amountST, staker, uuid, hashLock, unlockHash)
+      /*  , expectedBTValue = (amountST * conversionRate) / 10 ** conversionRateDecimals;
+     // assert.equal(decodedStakeEvent.BTValue, expectedBTValue, `Minted BT tokens are ${decodedStakeEvent.BTValue} but expected value is ${expectedBTValue}`);*/
+      let BTValue = 5
+        , redeemNonce = decodedStakeEvent.nonce + 1
+        , expectedST = BTValue * (10 ** conversionRateDecimals) / conversionRate;
+
+      let decodeUnStakeEvent = await tokenConversionUtils.processUnstake(openSTValue, uuid, staker, redeemNonce, BTValue, hashLock, registrar);
+      assert.equal(amountST, decodeUnStakeEvent.STValue, `Staked value token ${expectedST} is not equal to un-staked value token ${decodeUnStakeEvent.STValue}`)
+    });
+
+
+    it("Should receive expected number of branded token on multiple stake and one unstake request", async () => {
+
+      const lock = HashLock.getHashLock();
+      let hashLock = lock.l
+        , unlockHash = lock.s;
+
+      const stakeRequestAmount = [2, 2, 2, 29, 13, 23, 31, 57, 63, 70, 80];
+      let totalBTValue = 0;
+      let redeemNonce = 0;
+      let totalAmountST = 0;
+      let totalError = 0;
+      for (let i = 0; i < stakeRequestAmount.length; i++) {
+        let amountST = stakeRequestAmount[i]
+          ,
+          decodedStakeEvent = await tokenConversionUtils.processStake(valueToken, openSTValue, amountST, staker, uuid, hashLock, unlockHash);
+
+        totalAmountST += amountST;
+        let expectedBT = (amountST * conversionRate) / 10 ** conversionRateDecimals;
+        let errorBT = expectedBT - decodedStakeEvent.BTValue;
+        totalError += errorBT;
+       // console.log(`amount ST ${amountST}   amountBT  ${decodedStakeEvent.BTValue} ,  expected BT ${expectedBT} error  ${expectedBT - decodedStakeEvent.BTValue}`);
+
+        totalBTValue += decodedStakeEvent.BTValue;
+        redeemNonce = decodedStakeEvent.nonce + 1;
+      }
+
+     // console.log("Total Error BT  ", totalError);
+
+      totalBTValue = 5;
+      let decodeUnStakeEvent = await tokenConversionUtils.processUnstake(openSTValue, uuid, staker, redeemNonce, totalBTValue, hashLock, registrar);
+      assert.equal(totalAmountST, decodeUnStakeEvent.STValue, `Staked value token ${totalAmountST} is not equal to un-staked value token ${decodeUnStakeEvent.STValue}`)
     });
   });
 
