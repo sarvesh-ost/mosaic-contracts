0a22625be122ebab02e0014a8a8ae070937e69f4
Sarvesh Jain <sarvesh@ost.com>
2018-07-17 12:31:45 +0530

    Refactoring Renamed files and methods

--- contracts/TokenHolder.sol
+++ contracts/TokenHolder.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.4.23;
 
-contract VerifyECRecover {
+contract TokenHolder {
     bytes32 secret;
     mapping(address => bool) devices;
     mapping(address => mapping(bytes32 => bool)) usedSigs;
@@ -11,22 +11,23 @@ contract VerifyECRecover {
         return true;
     }
 
-    function isSigned(bytes32 msgHash, uint8 v, bytes32 r, bytes32 s) returns (bool) {
-        address _addr = ecrecover(msgHash, v, r, s);
-        require(devices[_addr] == true);
-        require(usedSigs[_addr][msgHash] == false);
-        usedSigs[_addr][msgHash] = true;
-        return true;
-    }
 
     function startSession(bytes32 sessionSecret) returns (bool){
         secret = sessionSecret;
         return true;
     }
 
-    function hash(bytes32 newScret) returns (bool){
+    function validateSession_1(bytes32 newScret) returns (bool){
         require(sha3(newScret) == secret);
         secret = newScret;
+        return true;
+    }
+
+    function validateSession_2(bytes32 msgHash, uint8 v, bytes32 r, bytes32 s) returns (bool) {
+        address _addr = ecrecover(msgHash, v, r, s);
+        require(devices[_addr] == true);
+        require(usedSigs[_addr][msgHash] == false);
+        usedSigs[_addr][msgHash] = true;
         return true;
     }
 
--- test/TokenHolder.js
+++ test/TokenHolder.js
@@ -1,4 +1,4 @@
-const VerifyECRecover = artifacts.require("./VerifyECRecover.sol");
+const TokenHolder = artifacts.require("./TokenHolder.sol");
 const utils = require("./lib/utils.js");
 
 const Web3 = require('web3')
@@ -7,18 +7,23 @@ const Web3 = require('web3')
 
 contract('Verify Ecrecover', function (accounts) {
 
-  let verifyECRecover = null;
+  let tokenHolder = null;
 
   describe('Verify Signer ', async () => {
 
     before(async () => {
-      verifyECRecover = await VerifyECRecover.new();
+      tokenHolder = await TokenHolder.new();
     })
 
     it('should verify signer', async function () {
       //console.log(web3);
+      let rawMsg = '0x8CbaC5e4d803bE2A3A5cd3DbE7174504c6DD0c1C';
+      //var rawMsg = '0xb30177d0f1279e3b428b0786027ba7ccae29a1e3915aaff7b0b27d6e83e7e38e';
+
+      //console.log("sha3  ", web3.utils.soliditySha3("0x5569044719a1ec3b04d0afa9e7a5310c7c0473331d13dc9fafe143b2c4e8148a"));
+
       let signer = accounts[0]
-        , msg = web3.utils.sha3('0x8CbaC5e4d803bE2A3A5cd3DbE7174504c6DD0c1C')
+        , msg = web3.utils.sha3(rawMsg)
         , prefix = `\x19Ethereum Signed Message:\n${32}`
         , fixed_msg_sha = web3.utils.soliditySha3(prefix, msg);
 
@@ -31,7 +36,14 @@ contract('Verify Ecrecover', function (accounts) {
 
       let v_decimal = web3.utils.toDecimal(v) + 27;
 
-      let result = await verifyECRecover.isSigned(signer, fixed_msg_sha, v_decimal, r, s);
+
+      console.log("signer", signer);
+      console.log("fixed_msg_sha", fixed_msg_sha);
+      console.log("v_decimal", v_decimal);
+      console.log("r", r);
+      console.log("s", s);
+
+      let result = await tokenHolder.validateSession_2(signer, fixed_msg_sha, v_decimal, r, s);
      /* assert.equal(result.logs[0].args.signer, signer);
       assert.equal(result.logs[0].args.status, true);*/
 
@@ -40,6 +52,19 @@ contract('Verify Ecrecover', function (accounts) {
       utils.clearReceipts();
     });
 
+
+    it('generate sha3', async function () {
+
+      let msg = '0x8CbaC5e4d803bE2A3A5cd3DbE7174504c6DD0c1C';
+      let prevSha = web3.utils.soliditySha3(msg);
+      for (let i = 0; i < 10; i++) {
+        console.log(prevSha);
+        prevSha = web3.utils.soliditySha3(prevSha);
+      }
+    });
+
   });
 
+
 });
+
