3ec78af6f1d564f71c2a48e0985ead9cde5e8d2c
Sarvesh Jain <sarvesh@ost.com>
2018-08-14 21:24:16 +0530

    Gateway and message bus implementation for request and accept stake

---
+++ contracts/gateway/Gateway.sol
@@ -0,0 +1,157 @@
+pragma solidity ^0.4.23;
+// Copyright 2018 OpenST Ltd.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// ----------------------------------------------------------------------------
+// Value chain: Gateway
+//
+// http://www.simpletoken.org/
+//
+// ----------------------------------------------------------------------------
+
+import "./ProtocolVersioned.sol";
+import "./OpenSTValueInterface.sol";
+import "./EIP20Interface.sol";
+import "./Owned.sol";
+import "./WorkersInterface.sol";
+import "./MessageBus.sol";
+
+contract Gateway {
+
+	event StakeRequested(
+		bytes32 requestHash,
+		uint256 amount,
+		address beneficiary,
+		uint256 fee
+	);
+
+	bytes32 uuid;
+	//Escrow address
+	address stakerAddress;
+	uint256 public bounty;
+	WorkersInterface public workers;
+
+
+	struct StakeRequest {
+		uint256 amount;
+		address beneficiary;
+		address staker;
+		uint256 fee;
+		uint256 nonce;
+		uint8 v;
+		bytes32 r;
+		bytes32 s;
+	}
+
+	mapping(bytes32 /* requesthash */ => StakeRequest) requests;
+	mapping(address /*staker*/ => uint256) nonces;
+	mapping(bytes32 /* msg digest*/ => MessageBus.Message) messages;
+	address brandedToken;
+	address messageBus;
+	MessageBus.MessageBox msgBox;
+
+	constructor (
+		WorkersInterface _workers,
+		uint256 _bounty,
+		bytes32 _uuid,
+		address _brandedToken,
+		address _messageBus
+	){
+		workers = _workers;
+		bounty = _bounty;
+		uuid = _uuid;
+		//todo deploy simple stake  contract
+		brandedToken = _brandedToken;
+		messageBus = _messageBus;
+	}
+
+	function requestStake(
+		uint256 _amount,
+		address _beneficiary,
+		uint256 _fee,
+		uint8 _v,
+		bytes32 _r,
+		bytes32 _s
+	)
+	returns (bytes32 requestHash_)
+	{
+		require(_amount != 0);
+		require(_beneficiary != address(0));
+		require(_fee > 0);
+		require(_v != 0);
+		//todo check possible value of v is, Possibly it's 27 and 28
+		require(_r != bytes32(0));
+		require(_s != bytes32(0));
+
+		require(EIP20Interface(brandedToken).transferFrom(msg.sender, this, _amount));
+
+		uint256 nonce = nonces[msg.sender];
+		nonces[msg.sender] = nonce ++;
+
+		requestHash_ = keccak256(msg.sender, nonce);
+
+		requests[requestHash_] = StakeRequest({
+			amount : _amount,
+			beneficiary : _beneficiary,
+			staker : msg.sender,
+			fee : _fee,
+			nonce : nonce,
+			v : _v,
+			r : _r,
+			s : _s
+			});
+
+		emit StakeRequested(
+			requestHash_,
+			_amount,
+			_beneficiary,
+			_fee
+		);
+	}
+
+	function acceptStake(
+		bytes32 requestHash,
+		bytes32 hashLock
+	)
+	returns (bytes32 messageHash_)
+
+	{
+		require(workers.isWorker(msg.sender));
+		require(requestHash != bytes32(0));
+		require(hashLock != bytes32(0));
+		require(requests[requestHash].amount != 0);
+
+		require(EIP20Interface(brandedToken).transferFrom(msg.sender, this, bounty));
+		StakeRequest request = requests[requestHash];
+		bytes32 intentHash = keccak256(
+								abi.encode(
+		                        	requestHash,
+									hashLock
+								)
+							 );
+		messages[requestHash] = MessageBus.Message({
+			intentHash : intentHash,
+			nonce : request.nonce,
+			//gasPrice:,
+			r : request.r,
+			s : request.s,
+			v : request.v,
+			sender : request.staker,
+			hashLock : hashLock
+			});
+		messageHash_ = MessageBus.declareMessage(msgBox, requestHash, messages[requestHash]);
+	}
+
+
+}
--- contracts/gateway/MessageBus.sol
+++ contracts/gateway/MessageBus.sol
@@ -1,6 +1,6 @@
-pragma solidity ^0.4.0;
+pragma solidity ^0.4.23;
 
-contract MessageBus {
+library MessageBus {
     enum MessageStatus {
         Undeclared,
         Declared,
@@ -19,7 +19,8 @@ contract MessageBus {
         // hash digest of intent message
         bytes32 intentHash;
         uint256 nonce;
-        uint256 gasPrice;
+        //todo removing gas price for now
+        //uint256 gasPrice;
         // signature (r, s, v) of intentHash, declaration nonce and gasPrice
         bytes32 r;
         bytes32 s;
@@ -28,6 +29,39 @@ contract MessageBus {
         bytes32 hashLock;
     }
 
+    function declareMessage(
+        MessageBox storage _messageBox,
+        bytes32 _messageTypeHash,
+        Message storage _message
+    )
+    external
+    returns (bytes32 messageHash_)
+    {
+        messageHash_ = messageDigest(_messageTypeHash, _message);
 
+        //todo intent hash is part of message hash and it's signed how? it's generate here only
+        require(_message.sender == ecrecover(messageHash_, _message.v, _message.r, _message.s));
+        require(_messageBox.outbox[messageHash_] == MessageStatus.Undeclared);
+
+        _messageBox.outbox[messageHash_] = MessageStatus.Declared;
+    }
+
+    function messageDigest(
+        bytes32 _messageTypeHash,
+        Message _message
+    )
+    internal
+    pure
+    returns (bytes32 /* messageHash */)
+    {
+        return keccak256(
+            abi.encode(
+                _messageTypeHash,
+                _message.intentHash,
+                _message.nonce//,
+            //_message.gasPrice
+            )
+        );
+    }
 
 }
